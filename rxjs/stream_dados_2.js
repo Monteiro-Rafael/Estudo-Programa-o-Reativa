8da292a93e7b42942b7c313fccd275f3227aa75d
Monteiro-Rafael <rafaeldonmonteiro@gmail.com>
2021-03-30 11:32:05 -0300

    stream dados 3, package.json and rxjs added

--- rxjs/stream_dados_2.js
+++ rxjs/stream_dados_2.js
@@ -28,6 +28,10 @@ const numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
 const temp1 = gerarElementos(numeros)
 
-temp1.iniciar(num => {
+const exec1 = temp1.iniciar(num => {
     console.log(Math.pow(2, num))
 })
+
+setTimeout(()=> {
+    exec1.parar()
+}, 4000)
