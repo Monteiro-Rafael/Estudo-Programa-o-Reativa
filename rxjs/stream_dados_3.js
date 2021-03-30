fdb69b1f6651c95d1fca791416a268bf5c00bc12
Monteiro-Rafael <rafaeldonmonteiro@gmail.com>
2021-03-30 11:46:29 -0300

    subscribe and unsubscribe

--- rxjs/stream_dados_3.js
+++ rxjs/stream_dados_3.js
@@ -1,1 +1,11 @@
-const rxjs = require('rxjs')
+const { interval } = require('rxjs')
+
+
+const gerarNumeros = interval(500)
+
+const sub1 = gerarNumeros.subscribe(num => {
+    console.log(Math.pow(2, num))
+})
+
+
+setTimeout(()=> sub1.unsubscribe(), 8000)
